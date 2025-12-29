4b272ca330733a1762c67b193d6a4606b423df2c
Tomasz Maciej Nowak <tmn505@gmail.com>
2025-12-29 02:24:01 +0100

    another try with alarm

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-mkdir alpine
-wget https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.2-aarch64.tar.gz
-bsdtar -x -f alpine-minirootfs-3.23.2-aarch64.tar.gz -C alpine
-resolvectl status eth0 | awk '/Current DNS Server.*/ {print "nameserver " $4}' >> alpine/etc/resolv.conf
-cat /run/systemd/resolve/stub-resolv.conf >> alpine/etc/resolv.conf
-cat alpine/etc/resolv.conf
-mount -t proc /proc alpine/proc/
-mount -t sysfs /sys alpine/sys/
-mount -o bind /dev alpine/dev/
-chroot alpine /bin/ash -c ". /etc/profile; ping -c 4 8.8.8.8; ping -c 4 mirror.archlinuxarm.org"
-chroot alpine /bin/ash -c ". /etc/profile; apk update; apk upgrade"
-chroot alpine /bin/ash -c ". /etc/profile; wget https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.2-aarch64.tar.gz; ls -l"
-#chroot alarm /bin/bash -c "source /etc/profile; pacman-key --init; pacman-key --populate archlinuxarm; pacman -S -y -u --noconfirm base-devel; locale-gen; uname -a"
+mkdir alarm
+wget http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz
+bsdtar -x -f ArchLinuxARM-aarch64-latest.tar.gz -C alarm
+#resolvectl status eth0 | awk '/Current DNS Server.*/ {print "nameserver " $4}' >> alarm/etc/resolv.conf
+#cat /run/systemd/resolve/stub-resolv.conf >> alarm/etc/resolv.conf
+mount -t proc /proc alarm/proc/
+mount -t sysfs /sys alarm/sys/
+mount -o bind /dev alarm/dev/
+mount -o bind /run alarm/run/
+cat alarm/etc/resolv.conf
+printf "en_US.UTF-8 UTF-8\n" > alarm/etc/locale.gen
+#chroot alarm /bin/bash -c ". /etc/profile; ping -c 4 8.8.8.8; ping -c 4 mirror.archlinuxarm.org"
+chroot alarm /bin/bash -c ". /etc/profile; wget https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.2-aarch64.tar.gz; ls -l"
+chroot alarm /bin/bash -c ". /etc/profile; wget http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz; ls -l"
+chroot alarm /bin/bash -c ". /etc/profile; pacman-key --init; pacman-key --populate archlinuxarm; pacman -S -y -u --noconfirm base-devel; locale-gen; uname -a"
