5d1c0f58f6f0e561de078ceaafddb7b056dd5330
Tomasz Maciej Nowak <tmn505@gmail.com>
2025-12-29 02:33:16 +0100

    another try with alarm 32 bit

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir alarm
-wget http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz
-bsdtar -x -f ArchLinuxARM-aarch64-latest.tar.gz -C alarm
+wget http://os.archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz
+bsdtar -x -f ArchLinuxARM-armv7-latest.tar.gz -C alarm
 #resolvectl status eth0 | awk '/Current DNS Server.*/ {print "nameserver " $4}' >> alarm/etc/resolv.conf
 #cat /run/systemd/resolve/stub-resolv.conf >> alarm/etc/resolv.conf
 mount -t proc /proc alarm/proc/
@@ -12,6 +12,6 @@ mount -o bind /run alarm/run/
 cat alarm/etc/resolv.conf
 printf "en_US.UTF-8 UTF-8\n" > alarm/etc/locale.gen
 #chroot alarm /bin/bash -c ". /etc/profile; ping -c 4 8.8.8.8; ping -c 4 mirror.archlinuxarm.org"
-chroot alarm /bin/bash -c ". /etc/profile; wget https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.2-aarch64.tar.gz; ls -l"
-chroot alarm /bin/bash -c ". /etc/profile; wget http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz; ls -l"
-chroot alarm /bin/bash -c ". /etc/profile; pacman-key --init; pacman-key --populate archlinuxarm; pacman -S -y -u --noconfirm base-devel; locale-gen; uname -a"
+linux32 chroot alarm /bin/bash -c ". /etc/profile; curl -L -O https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.2-aarch64.tar.gz; ls -l"
+linux32 chroot alarm /bin/bash -c ". /etc/profile; curl -L -O http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz; ls -l"
+linux32 chroot alarm /bin/bash -c ". /etc/profile; pacman-key --init; pacman-key --populate archlinuxarm; pacman -S -y -u --noconfirm base-devel; locale-gen; uname -a"
