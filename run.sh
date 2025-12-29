9f20e8ab657355a47150dd5d9b4da8a685442436
Tomasz Maciej Nowak <tmn505@gmail.com>
2025-12-29 01:58:58 +0100

    download something

--- run.sh
+++ run.sh
@@ -11,4 +11,5 @@ mount -t sysfs /sys alpine/sys/
 mount -o bind /dev alpine/dev/
 chroot alpine /bin/ash -c ". /etc/profile; ping -c 4 8.8.8.8; ping -c 4 mirror.archlinuxarm.org"
 chroot alpine /bin/ash -c ". /etc/profile; apk update; apk upgrade"
+chroot alpine /bin/ash -c ". /etc/profile; wget https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.2-aarch64.tar.gz; ls -l"
 #chroot alarm /bin/bash -c "source /etc/profile; pacman-key --init; pacman-key --populate archlinuxarm; pacman -S -y -u --noconfirm base-devel; locale-gen; uname -a"
